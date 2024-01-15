64faa958fe7e021e7d0a0e677f380b166f789b87
saivelamuri <saivelamuri26@gmail.com>
2024-01-15 11:31:58 +0100

    GlobalExceptionHandler Added

---
+++ src/main/java/com/example/NiRi/InvalidMethodArgumentException.java
@@ -0,0 +1,29 @@
+package com.example.NiRi;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InvalidMethodArgumentException extends RuntimeException {
+    private String errorKey;
+    public InvalidMethodArgumentException(String errorKey, String message) {
+        super(message);
+        this.errorKey = errorKey;
+    }
+
+    public String getErrorKey() {
+        return null;
+    }
+}
+
+
+
+
+
+
+
