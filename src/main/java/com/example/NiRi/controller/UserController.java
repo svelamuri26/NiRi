2b454cc0040c0c9e0db1f2aee0d230d7e69d716c
saivelamuri <saivelamuri26@gmail.com>
2024-01-15 22:28:44 +0100

    Added @Autowired Constructor in UserService class

--- src/main/java/com/example/NiRi/controller/UserController.java
+++ src/main/java/com/example/NiRi/controller/UserController.java
@@ -1,36 +1,39 @@
-package com.example.NiRi.Controller;
+package com.example.NiRi.controller;
 
 import com.example.NiRi.User;
 import com.example.NiRi.Repository.UserRepository;
 import com.example.NiRi.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    private UserRepository userRepository;
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
+
     @GetMapping("/all")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
     @GetMapping("/getByEmail")
     public User getUserByEmail(@RequestParam String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
     }
-
-
 }
